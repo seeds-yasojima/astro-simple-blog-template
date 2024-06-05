1012440ddad3d009676211df025ef87d5b7b2691
fumiya yasojima <yasojima@seeds-std.co.jp>
2024-06-05 11:32:37 +0900

    Reapply "Reapply "ブログの作り込み""
    This reverts commit 85119217ff24b198fa5e12b930105aaaa339a827.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -66,7 +66,14 @@ export default {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            maxWidth: "100%",
+          },
+        },
+      }),
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
 };
