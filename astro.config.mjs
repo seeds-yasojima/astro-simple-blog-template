9564759730feb85ea250783f28d6342a45abd999
fumiya yasojima <yasojima@seeds-std.co.jp>
2024-06-05 11:22:14 +0900

    Revert "Reapply "ブログの大枠を作成""
    This reverts commit e7f3d29e7c8fd53bcbb0c93cee8e3a3601b7cb48.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,4 @@
-import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
-
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [
-    react(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
-});
+export default defineConfig({});
