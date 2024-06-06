4119dacbc73c39d38e178949915c2f89b8cae0d0
fumiya yasojima <yasojima@seeds-std.co.jp>
2024-06-06 21:52:25 +0900

    修正

--- eslint.config.js
+++ eslint.config.js
@@ -1,35 +1,38 @@
 export default defineConfig({
   root: true,
+  /** env（環境）は、コードが実行される環境を指定します。 */
   env: { browser: true, es2020: true },
+  /** parser（パーサー）は、コードを解析するためのパーサーを指定します。 */
   parser: "@typescript-eslint/parser",
+  /** parserOptions（パーサーオプション）は、パーサーの設定を指定します。 */
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: "module",
     project: "./tsconfig.json",
   },
+  /** ignorePatterns（無視パターン）は、指定されたファイルを無視します。 */
+  ignorePatterns: ["dist", "eslint.config.js", ".prettierrc.mjs"],
+  /** plugins（プラグイン）は、ESLintに追加の機能を提供するプラグインを指定します。 */
   plugins: ["@typescript-eslint"],
+  /** extends（拡張）は、指定された設定を拡張します。 */
   extends: [
     "eslint:recommended",
-    "plugin:astro/recommended",
     "plugin:@typescript-eslint/recommended",
     "prettier",
   ],
-  rules: {
-    // add custom rules here
-  },
+  /** rules（ルール）は、ESLintのルールを指定します。 */
+  rules: {},
+  /** overrides（オーバーライド）は、指定されたファイルに対して別の設定を適用します。 */
   overrides: [
     {
       files: ["*.astro"],
       parser: "astro-eslint-parser",
+      extends: ["plugin:astro/recommended"],
       parserOptions: {
         parser: "@typescript-eslint/parser",
         extraFileExtensions: [".astro"],
       },
-      rules: {
-        // override/add rules settings here, such as:
-        // "astro/no-set-html-directive": "error"
-      },
+      rules: {},
     },
-    // ...
   ],
 });
