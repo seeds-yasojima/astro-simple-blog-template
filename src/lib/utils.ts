a8b94452c7e30dae31e874c6bdc739d258b41d67
fumiya yasojima <yasojima@seeds-std.co.jp>
2024-06-07 15:33:55 +0900

    軽微な修正

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,17 +1,26 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
-
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
 import timezone from "dayjs/plugin/timezone";
 
-export function formatDate(date: string): string {
-  dayjs.extend(utc);
-  dayjs.extend(timezone);
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
+/**
+ * clsx と tailwind-merge を組み合わせたユーティリティ関数
+ * @param inputs - クラス名
+ * @returns 結合されたクラス名
+ */
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+/**
+ * 日付をフォーマットする関数
+ * @param date - 日付
+ * @returns フォーマットされた日付
+ */
+export function formatDate(date: string): string {
   return dayjs.utc(date).tz("Asia/Tokyo").format("YYYY年MM月DD日");
 }
