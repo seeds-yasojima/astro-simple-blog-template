8dad2182e0f17284e3b2e02b708dd842003a5fb4
fumiya yasojima <yasojima@seeds-std.co.jp>
2024-06-07 13:30:06 +0900

    カテゴリー一覧・詳細の作り込み

---
+++ src/services/categories.ts
@@ -0,0 +1,36 @@
+import { client } from "@/lib/microcms";
+import type { MicroCMSQueries } from "microcms-js-sdk";
+import type { CommonContentsProps, ResponseProps } from "@/types";
+
+export type Categories = CommonContentsProps & {
+  name: string;
+};
+
+export type CategoriesResponse = ResponseProps<Categories>;
+
+/**
+ * カテゴリー一覧取得
+ * @params queries　クエリパラメータ
+ */
+export const getCategories = async (queries?: MicroCMSQueries) => {
+  return await client.get<CategoriesResponse>({
+    endpoint: "categories",
+    queries,
+  });
+};
+
+/**
+ * カテゴリー詳細取得
+ * @param contentId　コンテンツID
+ * @param queries　クエリパラメータ
+ */
+export const getCategoryDetail = async (
+  contentId: string,
+  queries?: MicroCMSQueries,
+) => {
+  return await client.getListDetail<Categories>({
+    endpoint: "categories",
+    contentId,
+    queries,
+  });
+};
